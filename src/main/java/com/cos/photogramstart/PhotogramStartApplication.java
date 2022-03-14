03fd1d7381d3362e3ba5f1daad3faffe8145c1e2
zxc9436 <zxc9436@naver.com>
2022-03-15 01:05:14 +0900

    last commit

--- src/main/java/com/cos/photogramstart/PhotogramStartApplication.java
+++ src/main/java/com/cos/photogramstart/PhotogramStartApplication.java
@@ -2,14 +2,20 @@ package com.cos.photogramstart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
 @SpringBootApplication
-public class PhotogramStartApplication {
+public class PhotogramStartApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(PhotogramStartApplication.class, args);
 	}
-
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {	
+		return builder.sources(PhotogramStartApplication.class);
+	}
 }
